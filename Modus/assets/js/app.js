59a22d9f549d4a9de7d8d24689365e7d8ab180ac
Kevin <natefr0st93@gmail.com>
2019-01-22 22:36:56 +0200

    Add files via upload

---
+++ Modus/assets/js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    var mobileBtn = $('.mobile-btn');
+    var mobileMenu = $('.mobile-menu');
+
+    // Show menu on click
+    
+    mobileBtn.on('click', function(){
+
+        if (mobileMenu.hasClass('open')) {
+            mobileMenu.removeClass('open');
+            mobileBtn.removeClass('active');
+        } else {
+            mobileMenu.addClass('open');
+            mobileBtn.addClass('active');
+        }
+        
+    });
+
+});
